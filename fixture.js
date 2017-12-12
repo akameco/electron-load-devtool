ac415b38484e5a5820bbdad3c9664d30542b4d86
akameco <akameco.t@gmail.com>
2017-12-13 06:21:21 +0900

    chore(npm): update config

--- fixture.js
+++ fixture.js
@@ -3,11 +3,11 @@ const electron = require('electron')
 const devtool = require('./')
 
 electron.app.on('ready', () => {
-	const win = new electron.BrowserWindow({ width: 400, height: 400 })
-	win.loadURL(`file://${__dirname}/fixture.html`)
+  const win = new electron.BrowserWindow({ width: 400, height: 400 })
+  win.loadURL(`file://${__dirname}/fixture.html`)
 
-	devtool(devtool.REDUX_DEVTOOLS)
-	devtool(devtool.VUEJS_DEVTOOLS)
+  devtool(devtool.REDUX_DEVTOOLS)
+  devtool(devtool.VUEJS_DEVTOOLS)
 
-	win.openDevTools()
+  win.openDevTools()
 })
--- readme.md
+++ readme.md
@@ -25,12 +25,12 @@ const electron = require('electron')
 const loadDevtool = require('electron-load-devtool')
 
 electron.app.on('ready', () => {
-	const win = new electron.BrowserWindow({ width: 400, height: 400 })
-	win.loadURL(`file://${__dirname}/index.html`)
+  const win = new electron.BrowserWindow({ width: 400, height: 400 })
+  win.loadURL(`file://${__dirname}/index.html`)
 
-	loadDevtool(loadDevtool.REDUX_DEVTOOLS)
+  loadDevtool(loadDevtool.REDUX_DEVTOOLS)
 
-	win.openDevTools()
+  win.openDevTools()
 })
 ```
 
